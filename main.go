4f05c1e70cd19ab55380991c39c185727c40f72b
Tyree Barron <tyree.barron@gmail.com>
2018-01-14 20:22:47 -0500

    wrote gets and post test functions

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 // Init books as a slice book Struct
 var books []Book
-
-
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -19,10 +19,24 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // For Testing purposes
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 
 }
 
