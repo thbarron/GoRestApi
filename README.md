1f2708fa39388873a931630d7e4516e318be917a
Tyree Barron <tyree.barron@gmail.com>
2018-01-15 10:36:49 -0500

    put and delete methods created.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GoRestApi
 Creating a REST Api in Go. Will see what else to add to it down the line.
+
+Some things that will happen eventually... adding a UI so that we can play around with listing, adding, changing and removing items. 
+
+Also will need a small db to store the items... Currently being stored in memory.
--- main.go
+++ main.go
@@ -41,11 +41,31 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			oldID := item.ID
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			json.NewDecoder(r.Body).Decode(&book)
+			book.ID = oldID
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
 }
 
 func main() {
